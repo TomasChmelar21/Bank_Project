df90d12cdc2b765916303ad884dcea4e20384db0
Tomas Chmelar <tomas.chmelar@tul.cz>
2023-04-24 21:29:28 +0200

    Add packages for project

---
+++ BankProject/src/test/java/cz/tul/Chmelar/BankMain/BankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tul.Chmelar.BankMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
