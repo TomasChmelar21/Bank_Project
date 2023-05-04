51ea9efe54527ec5c0bfb3e6d7090568144cfc9e
Tomas Chmelar <tomas.chmelar@tul.cz>
2023-05-04 17:04:20 +0200

    document comments in UserController

--- BankProject/src/main/java/cz/tul/Chmelar/Controllers/UserController.java
+++ BankProject/src/main/java/cz/tul/Chmelar/Controllers/UserController.java
@@ -15,6 +15,13 @@ import java.io.IOException;
 @Controller
 public class UserController {
 
+    /**
+     * return page where user can deposit money
+     *
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return page where user can deposit money
+     */
     @GetMapping("/deposit")
     public String deposit(Model model, Authentication authentication){
         String email = authentication.getName();
@@ -23,6 +30,13 @@ public class UserController {
         return "deposit";
     }
 
+    /**
+     * return page where user can do payment
+     *
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return page where user can do payment
+     */
     @GetMapping("/payment")
     public String payment(Model model, Authentication authentication){
         String email = authentication.getName();
@@ -31,6 +45,13 @@ public class UserController {
         return "payment";
     }
 
+    /**
+     * return page where user can open new account
+     *
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return page where user can open new account
+     */
     @GetMapping("/open_account")
     public String open_account(Model model, Authentication authentication){
         String email = authentication.getName();
@@ -39,6 +60,13 @@ public class UserController {
         return "open_account";
     }
 
+    /**
+     * return page where user can delete account
+     *
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return page where user can delete account
+     */
     @GetMapping("/delete_account")
     public String delete_account(Model model, Authentication authentication){
         String email = authentication.getName();
@@ -47,6 +75,16 @@ public class UserController {
         return "delete_account";
     }
 
+    /**
+     * process payment from payment page
+     *
+     * @param currency - currency in which user want to do payment
+     * @param amount - how much user want to send
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return account details page with processed payment
+     * @throws IOException
+     */
     @PostMapping("/process_payment")
     public String process_payment(@RequestParam("currency") String currency, @RequestParam(value = "amount", defaultValue = "0") double amount, Model model, Authentication authentication) throws IOException {
         String email = authentication.getName();
@@ -59,6 +97,16 @@ public class UserController {
         return "account_details";
     }
 
+    /**
+     * process deposit from deposit page
+     *
+     * @param currency - currency in which user want to do deposit
+     * @param amount - how much user want to get to account
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return account details page with processed deposit
+     * @throws IOException
+     */
     @PostMapping("/process_deposit")
     public String process_deposit(@RequestParam("currency") String currency, @RequestParam(value = "amount", defaultValue = "0") double amount, Model model, Authentication authentication) throws IOException {
         String email = authentication.getName();
@@ -71,6 +119,15 @@ public class UserController {
         return "account_details";
     }
 
+    /**
+     * process new account from new account page
+     *
+     * @param currency - currency in which user want to do create account
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return account details page with processed new account
+     * @throws IOException
+     */
     @PostMapping("/process_new_account")
     public String process_new_account(@RequestParam("currency") String currency, Model model, Authentication authentication) throws IOException{
         String email = authentication.getName();
@@ -83,6 +140,15 @@ public class UserController {
         return "account_details";
     }
 
+    /**
+     * process delete account from delete account page
+     *
+     * @param currency - currency in which user want to do delete account
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return account details page with processed delete account
+     * @throws IOException
+     */
     @PostMapping("/delete_old_account")
     public String delete_old_account(@RequestParam("currency") String currency, Model model, Authentication authentication) throws IOException{
         String email = authentication.getName();
