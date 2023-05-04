dde1dc645d321ec9e4057ca81cc62087b188bf4f
Tomas Chmelar <tomas.chmelar@tul.cz>
2023-05-04 17:07:28 +0200

    document comments in AppController

--- BankProject/src/main/java/cz/tul/Chmelar/Controllers/AppController.java
+++ BankProject/src/main/java/cz/tul/Chmelar/Controllers/AppController.java
@@ -10,17 +10,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
+    /**
+     * return index page
+     *
+     * @return index page
+     */
     @GetMapping("")
     public String index(){
         return "index";
     }
 
+    /**
+     * return login_index page
+     *
+     * @return login_index page
+     */
     @GetMapping("/login_index")
     public String login_index(){
 
         return "login_index";
     }
 
+    /**
+     * return account_details page
+     *
+     * @param model - holder for model attributes
+     * @param authentication - authentication of user
+     * @return account_details page
+     */
     @GetMapping("/account_details")
     public String useraccount(Model model, Authentication authentication){
         String email = authentication.getName();
@@ -28,11 +45,22 @@ public class AppController {
         model.addAttribute("user", user);
         return "account_details";
     }
+
+    /**
+     * return login page
+     *
+     * @return login page
+     */
     @GetMapping("/login")
     public String returnlogin(){
         return "login";
     }
 
+    /**
+     * return login_success page
+     *
+     * @return login_success page
+     */
     @GetMapping("/login_success")
     public String login_success(){
         return "login_success";
