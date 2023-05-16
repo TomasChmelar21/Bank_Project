4a6f3271bd43a7034364b9caa8ac065a008d11cf
TomasChmelar21 <101315477+TomasChmelar21@users.noreply.github.com>
2023-05-16 23:35:19 +0200

    Feature/develop (#7)

--- src/test/java/cz/tul/Chmelar/models/ExchangeRateRepositoryTest.java
+++ src/test/java/cz/tul/Chmelar/models/ExchangeRateRepositoryTest.java
@@ -153,41 +153,41 @@ class ExchangeRateRepositoryTest {
 
     @Test
     void getHtmlOfRates() throws IOException {
-        String expectedString = """
-                10.05.2023 #89
-                země|měna|množství|kód|kurz
-                Austrálie|dolar|1|AUD|14,430
-                Brazílie|real|1|BRL|4,295
-                Bulharsko|lev|1|BGN|11,971
-                Čína|žen-min-pi|1|CNY|3,088
-                Dánsko|koruna|1|DKK|3,144
-                EMU|euro|1|EUR|23,415
-                Filipíny|peso|100|PHP|38,392
-                Hongkong|dolar|1|HKD|2,731
-                Indie|rupie|100|INR|26,068
-                Indonesie|rupie|1000|IDR|1,452
-                Island|koruna|100|ISK|15,620
-                Izrael|nový šekel|1|ILS|5,833
-                Japonsko|jen|100|JPY|15,802
-                Jižní Afrika|rand|1|ZAR|1,142
-                Kanada|dolar|1|CAD|15,976
-                Korejská republika|won|100|KRW|1,615
-                Maďarsko|forint|100|HUF|6,314
-                Malajsie|ringgit|1|MYR|4,796
-                Mexiko|peso|1|MXN|1,207
-                MMF|ZPČ|1|XDR|28,852
-                Norsko|koruna|1|NOK|2,027
-                Nový Zéland|dolar|1|NZD|13,532
-                Polsko|zlotý|1|PLN|5,163
-                Rumunsko|leu|1|RON|4,757
-                Singapur|dolar|1|SGD|16,101
-                Švédsko|koruna|1|SEK|2,092
-                Švýcarsko|frank|1|CHF|23,969
-                Thajsko|baht|100|THB|63,513
-                Turecko|lira|1|TRY|1,094
-                USA|dolar|1|USD|21,384
-                Velká Británie|libra|1|GBP|26,973
-                """;
+        String expectedString = 
+    "10.05.2023 #89\n" +
+    "země|měna|množství|kód|kurz\n" +
+    "Austrálie|dolar|1|AUD|14,430\n" +
+    "Brazílie|real|1|BRL|4,295\n" +
+    "Bulharsko|lev|1|BGN|11,971\n" +
+    "Čína|žen-min-pi|1|CNY|3,088\n" +
+    "Dánsko|koruna|1|DKK|3,144\n" +
+    "EMU|euro|1|EUR|23,415\n" +
+    "Filipíny|peso|100|PHP|38,392\n" +
+    "Hongkong|dolar|1|HKD|2,731\n" +
+    "Indie|rupie|100|INR|26,068\n" +
+    "Indonesie|rupie|1000|IDR|1,452\n" +
+    "Island|koruna|100|ISK|15,620\n" +
+    "Izrael|nový šekel|1|ILS|5,833\n" +
+    "Japonsko|jen|100|JPY|15,802\n" +
+    "Jižní Afrika|rand|1|ZAR|1,142\n" +
+    "Kanada|dolar|1|CAD|15,976\n" +
+    "Korejská republika|won|100|KRW|1,615\n" +
+    "Maďarsko|forint|100|HUF|6,314\n" +
+    "Malajsie|ringgit|1|MYR|4,796\n" +
+    "Mexiko|peso|1|MXN|1,207\n" +
+    "MMF|ZPČ|1|XDR|28,852\n" +
+    "Norsko|koruna|1|NOK|2,027\n" +
+    "Nový Zéland|dolar|1|NZD|13,532\n" +
+    "Polsko|zlotý|1|PLN|5,163\n" +
+    "Rumunsko|leu|1|RON|4,757\n" +
+    "Singapur|dolar|1|SGD|16,101\n" +
+    "Švédsko|koruna|1|SEK|2,092\n" +
+    "Švýcarsko|frank|1|CHF|23,969\n" +
+    "Thajsko|baht|100|THB|63,513\n" +
+    "Turecko|lira|1|TRY|1,094\n" +
+    "USA|dolar|1|USD|21,384\n" +
+    "Velká Británie|libra|1|GBP|26,973";
+
         String url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt;?date=10.05.2023";
         String html = ExchangeRateRepository.getHtmlOfRates(url);
 
